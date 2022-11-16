62397b3532cb21614b6b32676fc9d1982d2ac5f7
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-16 20:25:20 +0200

    Inversions quadratic complexity

---
+++ src/lesson6/Inversions.java
@@ -0,0 +1,24 @@
+package lesson6;
+
+public class Inversions {
+
+
+    // Input:  A[] = [1, 9, 6, 4, 5]
+    // The inversion count is 5
+    // (9, 6), (9, 4), (9, 5), (6, 4), (6, 5)
+    // ((i < j) && arr[i] > arr[j])) == true
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1, 9, 6, 4, 5};
+        quadraticInversions(arr);
+    }
+
+    static int quadraticInversions(int[] arr) {
+        return 0;
+    }
+
+    static int linearLogInversions(int[] arr) {
+        return 0;
+    }
+
+}
