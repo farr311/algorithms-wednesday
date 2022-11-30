1290d025f21e4978b81a23da3eae5b7782757ebf
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-30 22:42:44 +0200

    Special stack solved

--- src/lesson8/stack/Runner.java
+++ src/lesson8/stack/Runner.java
@@ -1,9 +1,27 @@
 package lesson8.stack;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Runner {
     public static void main(String[] args) {
         SpecialStack stack = new SpecialStack();
 
-        stack
+        stack.push(10);
+        stack.push(5);
+        stack.push(7);
+        stack.push(6);
+        stack.push(1);
+        stack.push(4);
+        stack.push(4);
+
+        stack.print();
+
+        System.out.println(stack.getMin());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        System.out.println(stack.getMin());
     }
 }
--- src/lesson8/stack/SpecialStack.java
+++ src/lesson8/stack/SpecialStack.java
@@ -1,5 +1,8 @@
 package lesson8.stack;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Stack;
 
 public class SpecialStack {
@@ -10,29 +13,41 @@ public class SpecialStack {
     // *** Все эти операции SpecialStack должны быть O(1). Пространство O(n) ***
 
     private final Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
 
     public void push(int e) {
+        min = Math.min(min, e);
         stack.push(e);
     }
 
     public int pop() {
+        int value = stack.pop();
+        min = stack.stream().min(Comparator.naturalOrder()).orElse(Integer.MAX_VALUE);
 
+        return value;
     }
 
     public int peek() {
-
+        return stack.peek();
     }
 
     public boolean empty() {
-
+        return stack.empty();
     }
 
     public int size() {
+        return stack.size();
+    }
 
+    public void print() {
+        System.out.println(stack);
     }
 
     public int getMin() {
-
+        if (empty()) {
+            return -1;
+        }
+        return min;
     }
 
 }
