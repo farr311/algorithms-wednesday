56fb88a518e13a3b31bed5cbcc0b5e007e5b142c
farr311 <40473601+farr311@users.noreply.github.com>
2022-12-07 20:32:32 +0200

    Dynamic Array homework

--- src/lesson7/dynamicarray/DynamicArray.java
+++ src/lesson7/dynamicarray/DynamicArray.java
@@ -4,33 +4,44 @@ public class DynamicArray implements Dynamic {
 
     private int[] array;
 
+    private static final int DEFAULT_CAPACITY = 8;
+
+    private int capacity;
+    private int size = 0;
+
 
     public DynamicArray() {
-        //array = new int[...];
+        array = new int[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
     }
 
     // Конструктор с заданным начальным размером массива
     public DynamicArray(int capacity) {
-
+        array = new int[capacity];
+        this.capacity = capacity;
     }
 
     // Добавление элемента в конец массива
     @Override
     public void add(int value) {
-
+        if (size >= capacity) {
+            growSize(-1);
+        }
+        array[++size] = value;
     }
 
     // Добавление элемента по индексу
     @Override
     public void addAt(int index, int value) {
-
+        if (size >= capacity) {
+            growSize(index);
+        }
+        array[++size] = value;
     }
 
     // Удаление последнего элемента
     @Override
-    public void remove() {
-
-    }
+    public void remove() { array[--size] = 0; }
 
     // Удаление элемента по индексу
     @Override
@@ -39,8 +50,13 @@ public class DynamicArray implements Dynamic {
     }
 
     // Увеличивает размер массива. Вызывается когда нужно добавить элемент на позицию, больше, чем размер массива
-    private void growSize() {
-
+    private void growSize(int index) {
+        if (index == -1) {
+            // copy
+        } else {
+            // copy from 0 to index - 1
+            // then copy from index to size - 1 at [index + 1, size]
+        }
     }
 
     // Уменьшает размер массива до количества элементов
@@ -51,31 +67,32 @@ public class DynamicArray implements Dynamic {
 
     // Записывает новое значение в массив по индексу
     @Override
-    public void set(int index, int value) {
-
-    }
+    public void set(int index, int value) { array[index] = value; }
 
     // Получение элемента по индексу
     @Override
-    public int get(int index) {
-        return 0;
-    }
+    public int get(int index) { return array[index]; }
 
     // Удаление всех элементов из массива (массив меняет свое значение на дефолтное)
     @Override
     public void clear() {
-
+        array = new int[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+        size = 0;
     }
 
     // Проверяет, есть ли данный элемент в массиве
     @Override
     public boolean contains(int value) {
+        for (int element : array) {
+            if (element == value) {
+                return true;
+            }
+        }
         return false;
     }
 
     // Проверяет, является ли массив пустым
     @Override
-    public boolean isEmpty() {
-        return false;
-    }
+    public boolean isEmpty() { return size == 0; }
 }
